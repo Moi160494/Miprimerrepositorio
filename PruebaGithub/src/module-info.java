e9f91c7aab0741cf7eaff4b4f779262bb3112c9d
Moi <Moi@DESKTOP-EK9SD8Q>
2023-03-05 18:02:32 +0100

    Updated

---
+++ PruebaGithub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Moi
+ *
+ */
+module PruebaGithub {
+}
---
+++ PruebaGithub/src/pruebagithub/PGithub.java
@@ -0,0 +1,28 @@
+package pruebagithub;
+
+public class PGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenido a mi Repositorio de Github sincronizado con Eclipse");
+		System.out.println("Incluyo unos comandos que serán de utilidad");
+		
+		
+		/*…or create a new repository on the command line
+
+	echo "# Miprimerrepositorio" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/Moi160494/Miprimerrepositorio.git
+	git push -u origin main
+
+	…or push an existing repository from the command line
+
+	git remote add origin https://github.com/Moi160494/Miprimerrepositorio.git
+	git branch -M main
+	git push -u origin main*/
+	}
+
+}
